120a0d73c39d0fef1255030ea46bba1f432832f2
Rafiya <92360449+Rafiyathanvir@users.noreply.github.com>
2023-02-17 23:22:05 +0530

    Update PostPage.js

--- src/pages/PostPage.js
+++ src/pages/PostPage.js
@@ -9,7 +9,7 @@ export default function PostPage() {
   const {userInfo} = useContext(UserContext);
   const {id} = useParams();
   useEffect(() => {
-    fetch(`https://blog-app-ycfd.onrender.com/post/${id}`)
+    fetch(`https://blog-app-up.onrender.com/post/${id}`)
       .then(response => {
         response.json().then(postInfo => {
           setPostInfo(postInfo);
@@ -35,7 +35,7 @@ export default function PostPage() {
         </div>
       )}
       <div className="image">
-        <img src={`https://blog-app-ycfd.onrender.com/${postInfo.cover}`} alt=""/>
+                <img src={`https://blog-app-up.onrender.com/${postInfo.cover}`} alt=" "/>
       </div>
       <div className="content" dangerouslySetInnerHTML={{__html:postInfo.content}} />
     </div>
