22f8172100cbc9700827c236a11ec71fe4973478
Rafiya <92360449+Rafiyathanvir@users.noreply.github.com>
2023-02-17 23:13:05 +0530

    Update LoginPage.js

--- src/pages/LoginPage.js
+++ src/pages/LoginPage.js
@@ -9,7 +9,7 @@ export default function LoginPage() {
   const {setUserInfo} = useContext(UserContext);
   async function login(ev) {
     ev.preventDefault();
-    const response = await fetch('https://blog-app-ycfd.onrender.com/login', {
+    const response = await fetch('https://blog-app-up.onrender.com/login', {
       method: 'POST',
       body: JSON.stringify({username, password}),
       headers: {'Content-Type':'application/json'},
