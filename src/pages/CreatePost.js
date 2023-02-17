e5a5f54c502e3177467cada77d76416c4d092f8f
Rafiya <92360449+Rafiyathanvir@users.noreply.github.com>
2023-02-17 23:11:18 +0530

    Update CreatePost.js

--- src/pages/CreatePost.js
+++ src/pages/CreatePost.js
@@ -17,7 +17,7 @@ export default function CreatePost() {
     data.set('content', content);
     data.set('file', files[0]);
     ev.preventDefault();
-    const response = await fetch('https://blog-app-ycfd.onrender.com/post', {
+    const response = await fetch('https://blog-app-up.onrender.com/post', {
       method: 'POST',
       body: data,
       credentials: 'include',
