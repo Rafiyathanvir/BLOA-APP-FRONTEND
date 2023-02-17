b79af8b9a6dacc704ca1b667a8e2f05d7c8e8266
Rafiya <92360449+Rafiyathanvir@users.noreply.github.com>
2023-02-17 23:25:20 +0530

    Update Header.js

--- src/Header.js
+++ src/Header.js
@@ -5,7 +5,7 @@ import {UserContext} from "./UserContext";
 export default function Header() {
   const {setUserInfo,userInfo} = useContext(UserContext);
   useEffect(() => {
-    fetch('https://blog-app-ycfd.onrender.com/profile', {
+    fetch('https://blog-app-up.onrender.com/profile', {
       credentials: 'include',
     }).then(response => {
       response.json().then(userInfo => {
@@ -15,7 +15,7 @@ export default function Header() {
   }, []);
 
   function logout() {
-    fetch('https://blog-app-ycfd.onrender.com/logout', {
+    fetch('https://blog-app-up.onrender.com/logout', {
       credentials: 'include',
       method: 'POST',
     });
