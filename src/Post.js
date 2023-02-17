751f92ebef99d6ac14870dd62c2184842f469b9f
Rafiya <92360449+Rafiyathanvir@users.noreply.github.com>
2023-02-17 23:27:39 +0530

    Update Post.js

--- src/Post.js
+++ src/Post.js
@@ -7,7 +7,7 @@ export default function Post({_id,title,summary,cover,content,createdAt,author})
     <div className="post">
       <div className="image">
         <Link to={`/post/${_id}`}>
-          <img src={'https://blog-app-ycfd.onrender.com/'+cover} alt=""/>
+          <img src={'https://blog-app-up.onrender.com/'+cover} alt=""/>
         </Link>
       </div>
       <div className="texts">
