8966542904969742edbfea1a47bb6a3f39ff872b
Rafiya <92360449+Rafiyathanvir@users.noreply.github.com>
2023-02-17 23:12:06 +0530

    Update EditPost.js

--- src/pages/EditPost.js
+++ src/pages/EditPost.js
@@ -11,7 +11,7 @@ export default function EditPost() {
   const [redirect,setRedirect] = useState(false);
 
   useEffect(() => {
-    fetch('https://blog-app-ycfd.onrender.com/post/'+id)
+    fetch('https://blog-app-up.onrender.com/post/'+id)
       .then(response => {
         response.json().then(postInfo => {
           setTitle(postInfo.title);
@@ -31,7 +31,7 @@ export default function EditPost() {
     if (files?.[0]) {
       data.set('file', files?.[0]);
     }
-    const response = await fetch('https://blog-app-ycfd.onrender.com/post', {
+    const response = await fetch('https://blog-app-up.onrender.com/post', {
       method: 'PUT',
       body: data,
       credentials: 'include',
