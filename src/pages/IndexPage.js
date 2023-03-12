b33c0a1e848257777f6849b78378551a0bc0acbf
Rafiya <92360449+Rafiyathanvir@users.noreply.github.com>
2023-03-12 23:48:11 +0530

    Add files via upload

---
+++ src/pages/IndexPage.js
@@ -0,0 +1,20 @@
+import Post from "../Post";
+import {useEffect, useState} from "react";
+
+export default function IndexPage() {
+  const [posts,setPosts] = useState([]);
+  useEffect(() => {
+    fetch('https://blog-app-backend-nu.vercel.app/post').then(response => {
+      response.json().then(posts => {
+        setPosts(posts);
+      });
+    });
+  }, []);
+  return (
+    <>
+      {posts.length > 0 && posts.map(post => (
+        <Post {...post} />
+      ))}
+    </>
+  );
+}
