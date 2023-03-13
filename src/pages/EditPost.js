4252a605aad2fa38cb426ed3468872bf586447de
Rafiya <92360449+Rafiyathanvir@users.noreply.github.com>
2023-03-13 13:02:09 +0530

    Update EditPost.js

--- src/pages/EditPost.js
+++ src/pages/EditPost.js
@@ -31,7 +31,7 @@ export default function EditPost() {
     if (files?.[0]) {
       data.set('file', files?.[0]);
     }
-    const response = await fetch('https://blog-app-ycfd.onrender.com/post', {
+    const response = await fetch('https://blog-app-backend-nu.vercel.app/post', {
       method: 'PUT',
       body: data,
       credentials: 'include',
